74ec7bfd7002339ca09918fabef6010a99c72c2e
Simple Solution <office@simplesolution.at>
2018-08-17 09:58:55 +0200

    Update LdapUserNotFoundException.php

--- Classes/Exceptions/LdapUserNotFoundException.php
+++ Classes/Exceptions/LdapUserNotFoundException.php
@@ -1,5 +1,12 @@
 <?php
 namespace SimpleSolution\Ldap\Exceptions;
+
+/**
+ * LdapUserNotFoundException 
+ *
+ * @author Tobias Franek <tobias.franek@simplesolution.at>
+ * @license MIT - Simple Solution <office@simplesolution.at> 
+ */
 class LdapUserNotFoundException extends \Exception
 {
 }
